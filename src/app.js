f5e7703a40cb07aded15f1a829b37bb5c2901dcc
JosefaMiller <josefamillerb@gmail.com>
2023-02-23 18:42:04 +0000

    arreglando js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./index.html";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  window.Generatecard = function() {
+    var number = ['A',
+  "2",
+"3",
+"4",
+"5",
+"6",
+"7",
+"8",
+"9",
+"10",
+"J",
+"Q",
+"K"
+];
+    var symbol = ["♠", "♣", "♥", "♦"];
+
+    function rndmCard() {
+      let randomNumber = number [Math.floor(Math.random() * number.length)];
+      let randomSymbol = symbol [Math.floor(Math.random() * symbol.length)];
+      document.querySelector(".top-suit").innerHTML = randomSymbol;
+      document.querySelector(".number").innerHTML = randomNumber;
+      document.querySelector(".bottom-suit").innerHTML = randomSymbol;
+      if (randomSymbol == "♥" || randomSymbol == "♦");{
+        document.querySelector ("#card").getElementsByClassName.color = "red";
+      }
+      else (randomSymbol == "♣" || randomSymbol == "♠");{
+        document.querySelector ("#card").getElementsByClassName.color = "black";
+      }
+    }
+
+    rndmCard();
+  };
+  window.Generatecard();
 };
